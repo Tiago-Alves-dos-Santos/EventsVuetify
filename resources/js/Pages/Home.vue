460bca0b22d6f573ae0271b3118937e5a6edaa1d
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-08-04 17:53:31 -0300

    Inertia installed

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app app>
+        <v-container>
+            <v-alert
+            border="left"
+            color="indigo"
+            dark
+            >
+            I'm an alert with a border left type info
+            </v-alert>
+        </v-container>
+    </v-app>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,32 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 import vuetify from './vuetify';
+import { createInertiaApp } from '@inertiajs/vue2'
+//spa
+import { Link } from '@inertiajs/vue2'
+Vue.component('Link', Link);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-const app = new Vue({
-    el: '#app',
-    vuetify
-});
+
+ createInertiaApp({
+    progress: {
+        delay: 250,
+
+        // The color of the progress bar.
+        color: '#ff0a0a',
+
+        // Whether to include the default NProgress styles.
+        includeCSS: true,
+
+        // Whether the NProgress spinner will be shown.
+        showSpinner: false,
+    },
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props, plugin }) {
+      Vue.use(plugin)
+
+      new Vue({
+        vuetify,
+        render: h => h(App, props),
+      }).$mount(el)
+    },
+  })
