15e37e3d74252e977c22829840544f96a5d925b8
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-08-18 19:53:15 -0300

    added comments

--- app/Console/Commands/VerifyStatusEvent.php
+++ app/Console/Commands/VerifyStatusEvent.php
@@ -2,13 +2,10 @@
 
 namespace App\Console\Commands;
 
-use Carbon\Carbon;
-use Inertia\Inertia;
+
 use App\Models\Event;
 use App\Models\Historic;
-use App\Enums\EventStatus;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
 
 class VerifyStatusEvent extends Command
 {
@@ -44,18 +41,16 @@ class VerifyStatusEvent extends Command
     public function handle()
     {
         //chunk para evitar erro de tempo de execução,
-        Event::whereNotIn('status',['concluded','canceled'])->chunk(200, function ($events) {
+        Event::whereNotIn('status',['concluded','canceled'])->chunk(200, function ($events) { //eventos não cancelados e concluidos
             foreach ($events as $value) {
                 $old_status = $value->status;
-                $value->compareDatesGetEventStatus();
-                Log::info("Old status: $old_status e status $value->status valor: ". ($old_status != $value->status));
-                if($old_status != $value->status){
+                $value->compareDatesGetEventStatus();//atualizando status
+                if($old_status != $value->status){//caso novo valor diferente do antigo
                     $value->save();
                     Historic::create([
                         'registry' => "O evento {$value->name} foi editado pela rotina de ".Event::staticGetStatusInPortuguesBr($old_status)." para ". Event::staticGetStatusInPortuguesBr($value->status)
                     ]);
                 }
-
             }
         });
 
