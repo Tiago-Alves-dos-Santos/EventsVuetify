1a3988f7ea86a3df7916975fe01283b74b2d29b6
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-08-21 17:34:24 -0300

    test create event success

---
+++ tests/Feature/EventCrudTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EventCrudTest extends TestCase
+{
+    // use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_create()
+    {
+        $carbonStart = Carbon::now();
+        $carbonEnd= $carbonStart->copy();
+        $carbonEnd->addMinutes(30);
+
+        $dataRequest = [
+            'name' => 'User teste feature',
+            'date_start' => $carbonStart->format('Y-m-d'),
+            'date_end'  => $carbonEnd->format('Y-m-d'),
+            'time_start' => $carbonStart->format('H:i'),
+            'time_end' => $carbonStart->format('H:i'),
+            'text_color' => '#FFFFFF',
+            'text_background' => '#0008FF',
+        ];
+        $response = $this->post(route('event.create'), $dataRequest);
+        $this->assertDatabaseHas('events', $dataRequest);
+        $response->assertStatus(302);
+    }
+    public function test_update()
+    {
+        // $carbonStart = Carbon::now();
+        // $carbonEnd= $carbonStart->copy();
+        // $carbonEnd->addMinutes(30);
+
+        // $dataRequest = [
+        //     'name' => 'User teste feature',
+        //     'date_start' => $carbonStart->format('Y-m-d'),
+        //     'date_end'  => $carbonEnd->format('Y-m-d'),
+        //     'time_start' => $carbonStart->format('H:i'),
+        //     'time_end' => $carbonStart->format('H:i'),
+        //     'text_color' => '#FFFFFF',
+        //     'text_background' => '#0008FF',
+        // ];
+        // $response = $this->post(route('event.create'), $dataRequest);
+        // $this->assertDatabaseHas('events', $dataRequest);
+        // $response->assertStatus(302);
+    }
+}
