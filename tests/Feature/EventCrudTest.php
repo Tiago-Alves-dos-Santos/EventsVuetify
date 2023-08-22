e35483c2e2379c07569f52aafabd29c88aba5bce
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-08-21 21:57:01 -0300

    test update event success

--- tests/Feature/EventCrudTest.php
+++ tests/Feature/EventCrudTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Carbon\Carbon;
 use Tests\TestCase;
+use App\Models\Event;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -36,21 +37,25 @@ class EventCrudTest extends TestCase
     }
     public function test_update()
     {
-        // $carbonStart = Carbon::now();
-        // $carbonEnd= $carbonStart->copy();
-        // $carbonEnd->addMinutes(30);
+        $carbonStart = Carbon::now();
+        $carbonEnd= $carbonStart->copy();
+        $carbonEnd->addMinutes(30);
 
-        // $dataRequest = [
-        //     'name' => 'User teste feature',
-        //     'date_start' => $carbonStart->format('Y-m-d'),
-        //     'date_end'  => $carbonEnd->format('Y-m-d'),
-        //     'time_start' => $carbonStart->format('H:i'),
-        //     'time_end' => $carbonStart->format('H:i'),
-        //     'text_color' => '#FFFFFF',
-        //     'text_background' => '#0008FF',
-        // ];
-        // $response = $this->post(route('event.create'), $dataRequest);
-        // $this->assertDatabaseHas('events', $dataRequest);
-        // $response->assertStatus(302);
+        $event = Event::find(1);
+        $newEvent = [
+            'id' => $event->id,
+            'name' => 'User teste feature teste update, feito',
+            'old_date_start' => $event->date_start,
+            'date_start' => $carbonStart->format('Y-m-d'),
+            'date_end'  => $carbonEnd->format('Y-m-d'),
+            'time_start' => $carbonStart->format('H:i'),
+            'time_end' => $carbonStart->format('H:i'),
+            'text_color' => '#FFFFFF',
+            'text_background' => '#0008FF',
+        ];
+        $response = $this->put(route('event.update'), $newEvent);
+        unset($newEvent['old_date_start']);
+        $this->assertDatabaseHas('events', $newEvent);
+        $response->assertStatus(302);
     }
 }
