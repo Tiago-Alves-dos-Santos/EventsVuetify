5849d8637a525f860c6a16357cf0c24b8b111213
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-08-21 23:18:09 -0300

    tests completed

--- tests/Feature/EventCrudTest.php
+++ tests/Feature/EventCrudTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enums\EventStatus;
 use Carbon\Carbon;
 use Tests\TestCase;
 use App\Models\Event;
@@ -10,7 +11,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class EventCrudTest extends TestCase
 {
-    // use RefreshDatabase;
+    private int $id = 1;
     /**
      * A basic feature test example.
      *
@@ -19,15 +20,15 @@ class EventCrudTest extends TestCase
     public function test_create()
     {
         $carbonStart = Carbon::now();
-        $carbonEnd= $carbonStart->copy();
+        $carbonEnd = Carbon::now();
         $carbonEnd->addMinutes(30);
 
         $dataRequest = [
-            'name' => 'User teste feature',
+            'name' => 'Evento teste feature',
             'date_start' => $carbonStart->format('Y-m-d'),
             'date_end'  => $carbonEnd->format('Y-m-d'),
             'time_start' => $carbonStart->format('H:i'),
-            'time_end' => $carbonStart->format('H:i'),
+            'time_end' => $carbonEnd->format('H:i'),
             'text_color' => '#FFFFFF',
             'text_background' => '#0008FF',
         ];
@@ -41,21 +42,39 @@ class EventCrudTest extends TestCase
         $carbonEnd= $carbonStart->copy();
         $carbonEnd->addMinutes(30);
 
-        $event = Event::find(1);
+        $event = Event::find($this->id);
         $newEvent = [
             'id' => $event->id,
-            'name' => 'User teste feature teste update, feito',
+            'name' => 'Evento editado feature',
             'old_date_start' => $event->date_start,
             'date_start' => $carbonStart->format('Y-m-d'),
             'date_end'  => $carbonEnd->format('Y-m-d'),
             'time_start' => $carbonStart->format('H:i'),
-            'time_end' => $carbonStart->format('H:i'),
+            'time_end' => $carbonEnd->format('H:i'),
             'text_color' => '#FFFFFF',
             'text_background' => '#0008FF',
         ];
         $response = $this->put(route('event.update'), $newEvent);
         unset($newEvent['old_date_start']);
         $this->assertDatabaseHas('events', $newEvent);
+        $response->assertStatus(302);
+    }
+    public function test_cancel()
+    {
+        $response = $this->put(route('event.cancel'), ['id' => $this->id]);
+        $this->assertDatabaseHas('events', [
+            'status' => EventStatus::CANCELED->value
+        ]);
+        $response->assertStatus(302);
+    }
+    public function test_delete()
+    {
+        $response = $this->delete(route('event.delete', ['id' => $this->id]));
+        $response->assertStatus(302);
+    }
+    public function test_restore()
+    {
+        $response = $this->put(route('event.restore', ['id' => $this->id]));
         $response->assertStatus(302);
     }
 }
