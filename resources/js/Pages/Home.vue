24f379c34eb431b950b1058e2e92781bab289414
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-08-23 17:17:03 -0300

    home responsive

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,31 +1,57 @@
 <template>
     <layout-bottom-navigation>
-        <v-sheet tile height="54" class="d-flex flex-column">
-            <div style="width: 100%;" class="d-flex justify-center">
-                <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
-                    <v-icon>mdi-chevron-left</v-icon>
-                </v-btn>
-                <v-select v-model="type" :items="types" item-text="text" item-value="value" dense outlined hide-details
-                    class="ma-2" label="Formato"></v-select>
-                <v-select v-model="weekday" :items="weekdays" dense outlined hide-details label="Semana"
-                    class="ma-2"></v-select>
-                <v-btn icon class="ma-2" @click="$refs.calendar.next()">
-                    <v-icon>mdi-chevron-right</v-icon>
-                </v-btn>
-            </div>
-            <div style="width: 100%; text-transform: uppercase;" class="d-flex flex-column align-center justify-center">
-                {{ this.titleCalendar }}
-                <v-btn color="primary" class="mt-2" @click="$page.props.valueCalendar = Date.now()">
-                    Hoje
-                </v-btn>
-            </div>
-        </v-sheet>
-        <v-sheet height="600" style="margin-top: 90px;">
-            <v-calendar ref="calendar" v-model="$page.props.valueCalendar" color="primary" :weekdays="weekday" :type="type"
-                :events="$page.props.events" :event-overlap-threshold="30" :event-color="getEventColor"
-                :event-text-color="getEventTextColor" @change="getEvents" @click:event="clickEvent" @click:more="showMore"
-                @click:date="showMore"></v-calendar>
-        </v-sheet>
+        <div class="w-100" id="home">
+            <v-sheet tile height="54" class="d-flex flex-column">
+                <v-row>
+                    <v-col sm="12" md="12">
+                        <div style="width: 100%;" class="container_calendar_navigation">
+                            <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
+                                <v-icon>mdi-chevron-left</v-icon>
+                            </v-btn>
+                            <v-select v-model="type" :items="types" item-text="text" item-value="value" dense outlined
+                                hide-details class="ma-2" label="Formato"></v-select>
+                            <v-select v-model="weekday" :items="weekdays" dense outlined hide-details label="Semana"
+                                class="ma-2"></v-select>
+                            <v-btn icon class="ma-2" @click="$refs.calendar.next()">
+                                <v-icon>mdi-chevron-right</v-icon>
+                            </v-btn>
+                        </div>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <div class="container_calendar_navigation_sm">
+                        <!-- Botoes de navegação - breakpoint SM -->
+                        <div class="arrow_navigation">
+                            <v-col>
+                                <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
+                                    <v-icon>mdi-chevron-left</v-icon>
+                                </v-btn>
+                                <v-btn icon class="ma-2" @click="$refs.calendar.next()">
+                                    <v-icon>mdi-chevron-right</v-icon>
+                                </v-btn>
+                            </v-col>
+                        </div>
+                        <!-- FIM Botoes de navegação - breakpoint SM -->
+                        <div class="info_navigation">
+                            <span>{{ this.titleCalendar }}</span>
+                            <v-btn color="primary" class="mt-2" @click="$page.props.valueCalendar = Date.now()">
+                                Hoje
+                            </v-btn>
+                        </div>
+                    </div>
+                </v-row>
+            </v-sheet>
+            <v-row>
+                <v-col cols="12">
+                    <v-sheet height="600" class="row_calendar">
+                        <v-calendar ref="calendar" v-model="$page.props.valueCalendar" color="primary" :weekdays="weekday"
+                            :type="type" :events="$page.props.events" :event-overlap-threshold="30"
+                            :event-color="getEventColor" :event-text-color="getEventTextColor" @change="getEvents"
+                            @click:event="clickEvent" @click:more="showMore" @click:date="showMore"></v-calendar>
+                    </v-sheet>
+                </v-col>
+            </v-row>
+        </div>
     </layout-bottom-navigation>
 </template>
 <script>
@@ -48,25 +74,23 @@ export default {
             //valores de calendario
             type: 'month',
             weekday: [0, 1, 2, 3, 4, 5, 6],
-            // value: Date.now(),
         }
     },
     computed: {
         titleCalendar() {//retorna o mês e o ano do calendario, como string
-            let monthIndex = new Date(this.$page.props.valueCalendar).getMonth();
+            let monthIndex = new Date(this.$page.props.valueCalendar).getUTCMonth();
             let year = new Date(this.$page.props.valueCalendar).getFullYear();
             let monthNames = new Intl.DateTimeFormat('pt-BR', { month: 'long' }).format;
             return `${monthNames(new Date(0, monthIndex))} - ${year}`;
         }
     },
     methods: {
-        clickEvent({day:{date}}) {//clicar no evento do calendario, não implementada
-            console.log(date);
+        clickEvent({ day: { date } }) {//clicar no evento do calendario, não implementada
             this.$page.props.valueCalendar = date;
             this.type = 'day';
         },
         //retornando o evento em mudança de calendario
-        getEvents({ start, end }) {
+        getEvents({ end }) {
             this.$page.props.events;
         },
         //cores do evento
@@ -76,7 +100,7 @@ export default {
         getEventTextColor(event) {
             return event.textColor
         },
-        showMore({date}) {
+        showMore({ date }) {
             this.$page.props.valueCalendar = date;
             this.type = 'day';
         }
